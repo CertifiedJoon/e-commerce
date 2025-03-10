a23adb75ac0c64f9b2420132b09f25d73c252ce1
Joon_Thinkpad <mjypark1212@gmail.com>
2025-03-10 12:05:48 +0800

    Add sort filter search pagination

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,21 +6,17 @@ using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata.Ecma335;
 using Core.Interfaces;
 using Core.Specifications;
+using API.RequestHelpers;
 namespace API.Controllers;
 
-
-[ApiController] // amongst many, it adds auto serialization and binding.
-[Route("api/[controller]")]
-public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
 {
   [HttpGet]
-  public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort) // APIController binds the arguments to query params
+  public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery]ProductSpecParams specParams) // APIController binds the arguments (if not object) to query params
   {
-    var spec = new ProductSpecification(brand, type, sort);
+    var spec = new ProductSpecification(specParams);
 
-    var products = await repo.ListAsync(spec);
-
-    return Ok(products);
+    return await CreatePagedResult(repo, spec, specParams.PageIndex, specParams.PageSize);
   }
 
   [HttpGet("{id:int}")]
